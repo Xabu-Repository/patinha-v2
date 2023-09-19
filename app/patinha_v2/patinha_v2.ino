0bf203e952a93f6c269e60a645fbb6fd2ea4c3ab
Sr-Vinicius <rodriguesviniciusfreitas@gmail.com>
2023-09-19 14:47:04 -0300

    Unstable release

--- app/patinha_v2/patinha_v2.ino
+++ app/patinha_v2/patinha_v2.ino
@@ -19,7 +19,8 @@ using namespace Eigen;
 #define MG996R_MIN_POSITION  1*(4096*50/1000)   /* 1 milissecond pulse width */
 #define MG996R_MID_POSITION  1.6*(4096*50/1000) /* 1.5 milissecond pulse width */
 #define MG996R_MAX_POSITION  2.2*(4096*50/1000) /* 2 milisseconds pulse width */
-#define MG996R_RAD_TO_PWM(a) (round((1.2*(4096*50/1000)/(2*M_PI/3)))*a + (4096*50/1000))
+#define MG996R_RAD_TO_PWM(a) (round((1.2*(4096*50/1000)/(2*M_PI/3))*a + (4096*50/1000)))
+#define PS4_INT8_TO_DELTA(x) (1.0/63.0 * x)
 
 #define SENSOR    32
 #define GRIPPER_O 2
@@ -30,6 +31,9 @@ std::vector<float> o;
 Matrix<float, 4, 4> pose;
 Matrix<float, 4, 4> DELTA = Matrix4f::Zero();
 
+/* ps4 controller variables*/
+int8_t left_stick_x, left_stick_y, right_stick_y;
+bool L1_bt, L2_bt, R1_bt, R2_bt;
 
 void forward_kine(Matrix<float,4,4> &T, std::vector<float> o){
   T(0,0) = cos(o[1]+OFFSET2+o[2]+o[3]+OFFSET4)*cos(o[0]+OFFSET1)*cos(o[4]) - sin(o[0]+OFFSET1)*sin(o[4]);
@@ -56,7 +60,7 @@ void forward_kine(Matrix<float,4,4> &T, std::vector<float> o){
 
 std::vector<float> inverse_kine(Matrix<float,4,4> T){
   float o1, o2, o3, o4, o5, o234, C3, S3, num2, den2;
-  std::vector<float> angles;
+  std::vector<float> old_values = o;
   
   o1 = atan2(T(1,3), T(0,3));
   while(o1 < 0)
@@ -97,24 +101,26 @@ std::vector<float> inverse_kine(Matrix<float,4,4> T){
   while(o4 >= M_PI)
     o4 = o4 - M_PI;
   while(o5 >= M_PI)
-    o5 = o5 - M_PI;
-  
-  Serial.printf("\n\no1 = %f \n", o1);
-  Serial.printf("\no2 = %f \n", o2);
-  Serial.printf("\no3 = %f \n", o3);
-  Serial.printf("\no4 = %f \n", o4);
-  Serial.printf("\no5 = %f \n", o5);  
-  
-  return {o1,o2,o3,o4,o5}; 
+    o5 = o5 - M_PI;  
+
+  if(o[0]<=2*M_PI/3 || o[1]<=2*M_PI/3 || o[2]<=2*M_PI/3 || o[3]<=2*M_PI/3 || o[4]<=2*M_PI/3 ||
+     o[0]>=0 || o[1]>=0 || o[2]>=0 || o[3]>=0 || o[4]>=0)
+    return {o1,o2,o3,o4,o5};
+
+  return old_values;
 }
 
 
 void calculate_diff_operator(Matrix<float,4,4> &DELTA){
   float dx=0, dy=0, dz=0, drx=0, dry=0, drz=0;
   
-  dz = map(PS4.LStickX(), -128, 128, -0.5, 0.5);
-  dy = map(PS4.LStickY(), -128, 128, -0.5, 0.5);
-  dx = map(PS4.RStickY(), -128, 128, -0.5, 0.5);
+  dz = PS4_INT8_TO_DELTA(left_stick_y);
+  dy = PS4_INT8_TO_DELTA(left_stick_x);
+  dx = PS4_INT8_TO_DELTA(right_stick_y);
+
+  Serial.println(dx);
+  Serial.println(dy);
+  Serial.println(dz);
   
   DELTA << 0, -drz, dry, dx,
            drz, 0, -drx, dy,
@@ -123,12 +129,15 @@ void calculate_diff_operator(Matrix<float,4,4> &DELTA){
 }
 
 
-void calculate_new_pose(Matrix<float,4,4> T, Matrix<float,4,4> DELTA){
+void calculate_new_pose(Matrix<float,4,4> &T, Matrix<float,4,4> DELTA){
   Matrix<float,4,4> dT;
   dT = DELTA * T;
-  T = dT + T;
+  T = T + dT;
 }
 
+bool is_pose_in_workspace(std::vector<float> joints){
+  
+}
 
 void close_gripper(){
   digitalWrite(GRIPPER_O, LOW);
@@ -144,9 +153,34 @@ void open_gripper(){
 }
 
 
+void notify(){
+  L1_bt = PS4.L1();
+  R1_bt = PS4.R1();
+  L2_bt = PS4.L2();
+  R2_bt = PS4.R2();
+  left_stick_x = PS4.LStickX();
+  left_stick_y = PS4.LStickY();
+  right_stick_y = PS4.RStickY();
+}
+
+
+void onConnect(){
+  Serial.println("Connected!.");
+}
+
+
+void onDisConnect(){
+  Serial.println("Disconnected!.");    
+}
+
+
 void setup(){
   Serial.begin(115200);
-  PS4.begin("1a:2b:3c:01:01:01");  
+  PS4.attach(notify);
+  PS4.attachOnConnect(onConnect);
+  PS4.attachOnDisconnect(onDisConnect);
+  PS4.begin();
+  
   pinMode(GRIPPER_O, OUTPUT);
   pinMode(GRIPPER_S, OUTPUT);
   ledcSetup(0, PWM_FREQUENCY, PWM_RESOLUTION);
@@ -157,35 +191,25 @@ void setup(){
   //ledcSetup(5, PWM_FREQUENCY, PWM_RESOLUTION);
   ledcAttachPin(13, 0);
   ledcAttachPin(12, 1);
-  ledcAttachPin(14, 2); 
+  ledcAttachPin(14, 2);
   ledcAttachPin(27, 3);
   ledcAttachPin(26, 4);
   //ledcAttachPin(25, 5); 
   delay(100);
 
-  Serial.println("hemlo!");
-  o = {0, 0, 0, 0, 0};
+  o = {M_PI/3, M_PI/3, M_PI/2, M_PI/6, 0};
   forward_kine(pose, o);
-  for(int i=0; i<4; i++){
-    Serial.print("| ");
-    for(int j=0; j<4; j++)
-      Serial.printf("%f ", pose(i,j));
-    Serial.print("| \n");
-  }
-  Serial.print("\n");
-    for(int i=0; i<4; i++){
-    Serial.print("| ");
-    for(int j=0; j<4; j++)
-      Serial.printf("%f ", DELTA(i,j));
-    Serial.print("| \n");
-  }
-
   inverse_kine(pose);
+
+  for(int i=0; i<5; i++){
+    Serial.printf("%d ", (unsigned int) MG996R_RAD_TO_PWM(o[i]));
+    ledcWrite(i, (unsigned int) MG996R_RAD_TO_PWM( o[i] ));
+    }
 }
 
 
 void loop() { 
-  //if (PS4.isConnected()) {
+  if (PS4.isConnected()) {
   //  if (PS4.L1()) o[0] = o[0] - 0,1;  // Decremento base
   //  if (PS4.R1()) o[0] = o[0] + 0,1;  // Incremento base
   //  if (PS4.L2()) o[4] = o[4] - 0,1;  // Decremento pulso
@@ -193,17 +217,22 @@ void loop() {
   //  forward_kine(pose, o);
   
     /* joysticks linear moviments */
-  //  calculate_diff_operator(DELTA);
-  //  calculate_new_pose(pose, DELTA);
-  //  inverse_kine(pose);
-  //}
-  
-  for(int i=0; i<4; i++){
-    ledcWrite(i, MG996R_RAD_TO_PWM( o[i] ));
+    forward_kine(pose, o);
+    calculate_diff_operator(DELTA);
+    calculate_new_pose(pose, DELTA);
+    o = inverse_kine(pose);        
   }
-
-  analogRead(SENSOR)>600? close_gripper() : open_gripper();
-
-  delay(10000);
-  Serial.println("hemlo");
+  
+  Serial.printf("\n\no1 = %f \n", o[0]);
+  Serial.printf("\no2 = %f \n", o[1]);
+  Serial.printf("\no3 = %f \n", o[2]);
+  Serial.printf("\no4 = %f \n", o[3]);
+  Serial.printf("\no5 = %f \n", o[4]);  
+  for(int i=0; i<4; i++){
+    ledcWrite(i,(unsigned int) MG996R_RAD_TO_PWM( o[i] ));
+  }
+  
+  //analogRead(SENSOR)>600? close_gripper() : open_gripper();
+  
+  delay(50);
 }
